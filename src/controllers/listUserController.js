3aa08c4604205ef57f5194febbd9a20b04f87f5b
petrarca <pedropetrarcar@gmail.com>
2023-06-01 19:14:24 -0300

    feat/listUsers route is available and aceppts query params.

--- src/controllers/listUserController.js
+++ src/controllers/listUserController.js
@@ -1,10 +1,15 @@
 import Router from "koa-router";
+import listUsersService from "../services/listUsers";
+
 
 const listUsersRouter = new Router()
 
 listUsersRouter.get("/users", async (ctx) => {
+  const {page} = ctx.query
+  const response = listUsersService(page)
+  console.log(response)
   ctx.status = 200;
-  ctx.body = { total: 0, count: 0, rows: [] };
+  ctx.body = { total: response.totalUsers, page: response.currentPage, pageCount: response.totalPages, rows: [response.users] };
 });
 
 
